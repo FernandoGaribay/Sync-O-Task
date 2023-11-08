7137370ad220645f2ab70a8e887e4a3d040ac276
FernandoGaribay <garibaycejaf@gmail.com>
2023-11-08 00:08:55 -0600

    + Link de la activity_main a activity_dashboard

--- app/src/main/java/com/aristidevs/syncotask/MainActivity.kt
+++ app/src/main/java/com/aristidevs/syncotask/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.aristidevs.syncotask.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.aristidevs.syncotask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, dashboard::class.java)
+            startActivity(intent)
+            finish() // Cierra activity_main para que el usuario no pueda volver atrás
+        }
     }
+
+
 }
