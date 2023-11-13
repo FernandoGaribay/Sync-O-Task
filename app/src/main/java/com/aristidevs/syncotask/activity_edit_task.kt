482ce3a5dd5fa1125dd682e71f85fb4b32ca421b
FernandoGaribay <garibaycejaf@gmail.com>
2023-11-13 00:05:08 -0600

    + Activity Edit Task -> activity_edit_task

---
+++ app/src/main/java/com/aristidevs/syncotask/activity_edit_task.kt
@@ -0,0 +1,11 @@
+package com.aristidevs.syncotask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_edit_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_task)
+    }
+}
