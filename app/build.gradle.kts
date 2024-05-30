3f8efaa189546eeb2b09c2ef97c3e58bc0e71899
Bonnielamb6 <edgartrabajos2205@gmail.com>
2024-05-30 17:02:28 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,11 +44,17 @@ dependencies {
 
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("androidx.cardview:cardview:1.0.0")
+
     implementation("androidx.activity:activity:1.8.0")
+
+    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation ("com.google.code.gson:gson:2.8.9")
+
 }
