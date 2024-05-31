8503a05ac5dbe0ed3026b4996708b22f5821cb86
Fernando Garibay <garibaycejaf@gmail.com>
2024-05-30 18:27:37 -0600

    + ActivityMyNotes evento boton btnMyTasks para regresar al dashboard

--- app/src/main/java/com/aristidevs/syncotask/ActivityMyNotes.kt
+++ app/src/main/java/com/aristidevs/syncotask/ActivityMyNotes.kt
@@ -1,19 +1,29 @@
 package com.aristidevs.syncotask
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.aristidevs.syncotask.activity.DashboardActivity
 import com.aristidevs.syncotask.adapters.myNotesAdapter
 import com.aristidevs.syncotask.interfaces.onNoteClickListener
 import com.aristidevs.syncotask.objects.NoteProvider
 import com.aristidevs.syncotask.objects.Notes
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ActivityMyNotes : AppCompatActivity(), onNoteClickListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var notesProvider: NoteProvider
     private lateinit var adapter: myNotesAdapter
+
+    private lateinit var btnMyTasks: LinearLayout
+    private lateinit var btnMyNotes: LinearLayout
+    private lateinit var btnPomodoro: LinearLayout
+    private lateinit var btnCalendar: LinearLayout
+    private lateinit var btnCreateTask: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +36,30 @@ class ActivityMyNotes : AppCompatActivity(), onNoteClickListener {
     private fun initViews() {
         notesProvider = NoteProvider()
 
+        btnMyTasks = findViewById(R.id.dashboard_btnMyTasks)
+        btnMyNotes = findViewById(R.id.dashboard_btnMyNotes)
+        btnPomodoro = findViewById(R.id.dashboard_btnPomodoro)
+        btnCalendar = findViewById(R.id.dashboard_btnCalendar)
+        btnCreateTask = findViewById(R.id.btnCreateNote)
+
         recyclerView = findViewById(R.id.savedNotes)
     }
 
     private fun setClickListeners() {
+        btnMyTasks.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish() // Cierra el activity para que el usuario no pueda volver atrás
+        }
+        btnMyNotes.setOnClickListener {
+
+        }
+        btnPomodoro.setOnClickListener {
+
+        }
+        btnCalendar.setOnClickListener {
+
+        }
         notesProvider.setOnDataChangedCallback {
             // Filtra las tareas que coinciden con las restricciones
             var filteredNotes = notesProvider.listNotes.toMutableList()
